e6417a7e856db86fa47d7a8aacf8c87df2d75344
Pasquale <lonetti@paciunzio.istruzioneer.it>
2022-05-29 17:27:43 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+Indagine statistica da parte di una banca su un campione di 10 clienti sugli ultimi 10 movimenti di prelievo contanti.
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+
+}
